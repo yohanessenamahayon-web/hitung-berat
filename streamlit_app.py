c57f37568ad18d31a11a4413b22bd8a00f2b4e7b
yohanessenamahayon-web <yohanessenamahayon@gmail.com>
2026-02-13 10:46:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 t = st.number_input("Masukan Tinggi) (cm) ",0)
 st.title("Menghitung :blue[Berat Badan] :Manusia:")
 r = st.number_input("Masukan Berat Badan) (cm) ",0)
-if st.button("Hitung Volume", type=" primary"):
+if st.button("Hitung Volume", type="primary"):
   v = math.pi*(r**2)*t
   st.success(f"volume berat adalah {v:. 2f}")

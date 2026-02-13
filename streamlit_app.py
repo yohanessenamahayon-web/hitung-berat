f8e6973ada3455e209ce3d290a2077eaf29981fe
yohanessenamahayon-web <yohanessenamahayon@gmail.com>
2026-02-13 10:35:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
-t = st.number_input("Masukan Tinggi Badan") (cm) ",0)
+t = st.number_input("Masukan Tinggi) (cm) ",0)
 st.title("Menghitung :blue[Berat Badan] :Manusia:")
-r = st.number_input("Masukan Berat Badan") (cm) ",0)
+r = st.number_input("Masukan Berat Badan) (cm) ",0)
